0ad65403939d62bae8fbc6e14b0ec4f139852116
Nicholas Interest <NicholasInterest1@users.noreply.github.com>
2019-09-16 18:46:10 -0400

    MVP Met

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,24 @@
-// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Controls from "./Controls";
+
+test("The Controls component that shows the buttons to toggle locked & closed states", () => {
+  const { getAllByRole } = render(<Controls />);
+  getAllByRole("button");
+});
+
+test("The Controls component that changes the text to reflect the state the door will be in if clicked", () => {
+  expect(true).toBe(false);
+});
+
+test("The Controls component that disables the closed button when the gate is locked", () => {
+  const control = render(<Controls lock={true} closed={true} />);
+  const btn = control.getByText(/open gate/i);
+  expect(btn.disabled).toBe(true);
+});
+
+test("The Controls component that disables the locked button when the gate is open", () => {
+  const control = render(<Controls lock={false} closed={false} />);
+  const btn = control.getByText(/lock gate/i);
+  expect(btn.disabled).toBe(true);
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,32 @@
-// Test away
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Controls from "../controls/Controls";
+import Display from "../display/Display";
+import Dashboard from "./Dashboard";
+
+test("should if correct match the snapshot", () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+})
+
+test("The Gate default is unlocked & open", () => {
+    const dashboard = render(<Dashboard />);
+    expect(dashboard.getByText(/open/i));
+    expect(dashboard.getByText(/unlocked/i));
+})
+
+test("The Gate can't be opened or closed when it is locked", () => {
+    const control = render(<Controls locked={true} closed={true} />);
+    expect(control.queryByText(/close gate/i)).toBe(null);
+
+    const btn = control.getByText(/open gate/i);
+    expect(btn.disabled).toBe(true);
+})
+
+test("The dashboard has controls", () => {
+  render(<Controls />);
+});
+
+test("The dashboard has a display", () => {
+  render(<Display />);
+});
